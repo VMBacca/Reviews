480c8ee26fedf74531472eb435767661449844c6
Vinícius Marcondes Bacca <vinimbacca@gmail.com>
2022-04-18 15:20:05 -0300

    Criando Repositório

---
+++ src/data.js
@@ -0,0 +1,36 @@
+import red from "./img/red.jpg";
+
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image: red,
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "bob willis",
+    job: "web designer",
+    image: "https://wallpaperaccess.com/full/2230766.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://st.depositphotos.com/1034986/1970/i/600/depositphotos_19709869-stock-photo-puppet-man-listening-to-music.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "andrea anderson",
+    job: "the boss",
+    image:
+      "https://img.freepik.com/free-photo/pretty-woman-looking-desperate-frustrated-stressed-student-concept_1194-342502.jpg?size=626&ext=jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+export default reviews;
