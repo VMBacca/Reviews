08048d3b5b366f0d5fded284251839fdef07659f
Vinícius Marcondes Bacca <vinimbacca@gmail.com>
2022-04-19 13:52:54 -0300

    adding new layout and new reviews

--- src/data.js
+++ src/data.js
@@ -31,6 +31,22 @@ const reviews = [
       "https://img.freepik.com/free-photo/pretty-woman-looking-desperate-frustrated-stressed-student-concept_1194-342502.jpg?size=626&ext=jpg",
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
+  {
+    id: 5,
+    name: "Fitzpatrick O'Callaghan",
+    job: "accountant",
+    image:
+      "https://t4.ftcdn.net/jpg/03/25/67/89/360_F_325678993_phZoV7APkGT4zVPRt2jDDmyJiG6UwVRb.jpg",
+    text: "Qui quia laudantium 33 iste odio non obcaecati exercitationem. Aut commodi repellendus vel maiores ipsam et sunt dolor et rerum nesciunt? Eum internos accusamus et impedit ullam qui internos voluptates aut aliquam quisquam et voluptate expedita eum porro corrupti.",
+  },
+  {
+    id: 6,
+    name: "ava peterson",
+    job: "security",
+    image:
+      "https://media.istockphoto.com/photos/eccentric-senior-lady-with-cool-attitude-portrait-picture-id962240854?k=20&m=962240854&s=612x612&w=0&h=akx4pOtJsuwNlq3jVVC9rWj9e--pj-pYLLeFkBuvBfo=",
+    text: "Et dignissimos nemo est culpa earum aut internos obcaecati et perferendis dignissimos ut voluptatum asperiores aut ipsam unde. Et earum unde hic quia consequatur eum vitae rerum ut maxime corporis.",
+  },
 ];
 
 export default reviews;
